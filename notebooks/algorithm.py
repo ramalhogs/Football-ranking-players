ec89e3901c6f704ecc7ea747767bbb694ac84ba3
ramalhogs <gustavoramalho384@gmail.com>
2024-10-10 15:58:33 -0300

    add updates in new players with the mean of the team

--- notebooks/algorithm.py
+++ notebooks/algorithm.py
@@ -1,6 +1,4 @@
-
 import numpy as np
-
 
 class FootballMatchRating:
     def __init__(self, match_data, team_1_name, team_2_name):
@@ -10,7 +8,6 @@ class FootballMatchRating:
 
         self.team_1 = self.match_data[self.match_data['team'] == self.team_1_name]
         self.team_2 = self.match_data[self.match_data['team'] == self.team_2_name]
-
 
     def update_k_q_values(self, player):
         if (player['games_played'] < 30 or player['age'] < 18) and player['rating'] < 2300:
@@ -37,6 +34,11 @@ class FootballMatchRating:
 
         Mmax = 90
         for index, player in team_1.iterrows():
+            # Se o jogador for novo, ajusta seu rating para a média do time
+            if player['rating'] == 1500:
+                team_rating = self.calculate_team_rating(team_1)
+                team_1.loc[index, 'rating'] = team_rating
+
             Ea = self.expected_score(player['rating'], Rb)
             Da = player['Goals For'] - player['Goals Against']
 
